9863f9c1c8550caa16f23a2278a49d79fd3a8ed0
Vito <vito.zupanic1@student.um.si>
2023-05-15 15:32:38 +0200

    dodal firebase functione

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,12 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
 const {initializeApp} = require("firebase-admin/app");
+const {getFirestore} = require("firebase-admin/firestore");
+
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const express = require("express");
 
 initializeApp();
+const db = getFirestore();
 const app = express();
 app.use((req, res, next) => {
     res.set("Access-Control-Allow-Origin", "*");
@@ -55,8 +49,18 @@ app.post("/test", (req, res) => {
     });
 });
 
-app.put("/", (req, res) => {
-    res.status(200).send("PUT request received");
+app.post("/test1", async (req, res) => {
+    const docRef = db.collection("test2").doc("alovena");
+
+    await docRef.set({
+        first: 'Ada',
+        last: 'Lovelace',
+        born: 1815
+    }).then((snapshot) => {
+        res.status(200).send(`POST request received ${snapshot}`);
+    }).catch((error) => {
+        res.status(500).send(`POST request failed ${error}`);
+    });
 });
 
 app.delete("/", (req, res) => {
