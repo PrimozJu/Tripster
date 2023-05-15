22010a076c59927801a7865600b9307f7a1196e1
PrimozJu <primoz.juric@gmail.com>
2023-05-15 12:53:08 +0200

    izgled stays-ov

---
+++ frontend/src/pages/LandingPages/Stays/ApartmentList.js
@@ -0,0 +1,32 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+
+function ApartmentList({ data }) {
+  console.log("nanaanan");
+  return (
+    <div className="container">
+      <div className="row row-cols-1 row-cols-md-2 row-cols-lg-4">
+        {data.map((result) => (
+          <div key={result.id} className="col mb-4">
+            <div className="card h-100">
+              <img src={result.images[0]} className="card-img-top" alt={result.name} />
+              <div className="card-body">
+                <h5 className="card-title">{result.name}</h5>
+                <p className="card-text">
+                  <strong>Address:</strong> {result.address}, {result.city}
+                </p>
+                <p className="card-text">
+                  <strong>Price:</strong> {result.price.total} {result.price.currency}
+                </p>
+                <a href={result.url} className="btn btn-primary">Click here to book</a>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default ApartmentList;
+
--- frontend/src/pages/LandingPages/Stays/Stays.js
+++ frontend/src/pages/LandingPages/Stays/Stays.js
@@ -1,7 +1,111 @@
-import React from "react"
+import React, { useState } from "react"
 import FormAirBnb from "./FormAirBnb";
 import axios from "axios";
+import DefaultNavbar from "examples/Navbars/DefaultNavbar";
+import routes from "routes";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Card from "@mui/material/Card";
+import bgImage from "assets/images/tnf.jpg";
+import ApartmentList from "./ApartmentList";
+// Material Kit 2 React components
+import MKBox from "components/MKBox";
+import MKTypography from "components/MKTypography";
+
+
+
+/* 
 async function handleForm(params) {
+  console.log(params);
+  const options = {
+    method: "GET",
+    url: "https://airbnb13.p.rapidapi.com/search-location",
+    params: {
+      location: params.location,
+      checkin: params.checkin,
+      checkout: params.checkout,
+      adults: params.adults,
+      children: params.children,
+      infants: params.infants,
+      pets: params.pets,
+      page: params.page,
+      currency: params.currency,
+    },
+    headers: {
+      "X-RapidAPI-Key": "0e529198bdmshaba36f78a36a9a5p1a9db0jsn082565d2fdf8",
+      "X-RapidAPI-Host": "airbnb13.p.rapidapi.com",
+    },
+  };
+
+  const response = await axios.request(options);
+  console.log(response.data.results);
+  
+   */
+ /*  try {
+
+    const dataDiv = document.getElementById("data");
+    dataDiv.innerHTML = "";
+
+    // Loop through each result and create a new container div element for each one
+    response.data.results.forEach((result) => {
+      // Create container div
+      const containerDiv = document.createElement("div");
+      containerDiv.setAttribute("class", "container");
+
+      // Get the required attributes
+      const { id, address, city, images, name, price, url } = result;
+
+      // Add id, address, city, and name to the container div
+      const idDiv = document.createElement("div");
+      idDiv.textContent = `id: ${id}`;
+      containerDiv.appendChild(idDiv);
+
+      const addressDiv = document.createElement("div");
+      addressDiv.textContent = `address: ${address}, ${city}`;
+      containerDiv.appendChild(addressDiv);
+
+      const nameDiv = document.createElement("div");
+      nameDiv.textContent = `name: ${name}`;
+      containerDiv.appendChild(nameDiv);
+
+      // Add images to the container div with right-click and left-click functionality
+      const image = document.createElement("img");
+      image.setAttribute("src", images[0]);
+
+      containerDiv.appendChild(image);
+
+      // Add prices to the container div
+      const pricesDiv = document.createElement("div");
+      pricesDiv.textContent = `price: ${price.total} ${price.currency}`;
+      containerDiv.appendChild(pricesDiv);
+
+      // Add url to the container div with click functionality
+      const urlDiv = document.createElement("div");
+      urlDiv.innerHTML = `<a href="${url}">Click here to book</a>`;
+      containerDiv.appendChild(urlDiv);
+
+      dataDiv.appendChild(containerDiv); // Add containerDiv to the dataDiv
+    }
+
+
+
+    );
+
+    
+
+    
+  } catch (error) {
+    console.log("napaka");
+    console.error(error);
+  } 
+}*/
+
+
+const Stays = () => {
+  //let response;
+  const [response, setResponse] = useState(undefined)
+
+  async function handleForm(params) {
     console.log(params);
     const options = {
       method: "GET",
@@ -23,69 +127,93 @@ async function handleForm(params) {
       },
     };
   
-    try {
-      const response = await axios.request(options);
-      console.log(response.data.results);
-  
-      const dataDiv = document.getElementById("data");
-      dataDiv.innerHTML = "";
-  
-      // Loop through each result and create a new container div element for each one
-      response.data.results.forEach((result) => {
-        // Create container div
-        const containerDiv = document.createElement("div");
-        containerDiv.setAttribute("class", "container");
-  
-        // Get the required attributes
-        const { id, address, city, images, name, price, url } = result;
-  
-        // Add id, address, city, and name to the container div
-        const idDiv = document.createElement("div");
-        idDiv.textContent = `id: ${id}`;
-        containerDiv.appendChild(idDiv);
-  
-        const addressDiv = document.createElement("div");
-        addressDiv.textContent = `address: ${address}, ${city}`;
-        containerDiv.appendChild(addressDiv);
-  
-        const nameDiv = document.createElement("div");
-        nameDiv.textContent = `name: ${name}`;
-        containerDiv.appendChild(nameDiv);
-  
-        // Add images to the container div with right-click and left-click functionality
-        const image = document.createElement("img");
-        image.setAttribute("src", images[0]);
-  
-        containerDiv.appendChild(image);
-  
-        // Add prices to the container div
-        const pricesDiv = document.createElement("div");
-        pricesDiv.textContent = `price: ${price.total} ${price.currency}`;
-        containerDiv.appendChild(pricesDiv);
-  
-        // Add url to the container div with click functionality
-        const urlDiv = document.createElement("div");
-        urlDiv.innerHTML = `<a href="${url}">Click here to book</a>`;
-        containerDiv.appendChild(urlDiv);
-  
-        dataDiv.appendChild(containerDiv); // Add containerDiv to the dataDiv
-      });
-    } catch (error) {
-      console.log("napaka");
-      console.error(error);
-    }
-  }
+     const respons = await axios.request(options);
+     setResponse(respons)
+    console.log(respons.data.results);
+  }  
 
 
-const Stays = () =>{
 
-    return(
-        <div >
-             {/*Tu pride se meni */}
-            <FormAirBnb  handleForm={handleForm} />
-            <div id="data"></div>
-            
-        </div>
-    )
+
+
+
+
+  return (
+    <div >
+      <DefaultNavbar
+        routes={routes}
+        action={{
+          type: "internal",
+          route: "/user/login",
+          label: "Login",
+          color: "info",
+        }}
+        sticky
+      />
+
+      <MKBox
+        minHeight="75vh"
+        width="100%"
+        sx={{
+          backgroundImage: `url(${bgImage})`,
+          backgroundSize: "cover",
+          backgroundPosition: "top",
+          display: "grid",
+          placeItems: "center",
+        }}
+      >
+        <Container>
+          <Grid container item xs={12} lg={7} justifyContent="center" mx="auto">
+            <MKTypography
+              variant="h1"
+              color="dark blue"
+              mt={-6}
+              mb={1}
+              sx={({ breakpoints, typography: { size } }) => ({
+                [breakpoints.down("md")]: {
+                  fontSize: size["3xl"],
+                },
+              })}
+            >
+              Tripster{" "}
+            </MKTypography>
+            <MKTypography
+              variant="body1"
+              color="dark blue"
+              textAlign="center"
+              px={{ xs: 6, lg: 12 }}
+              mt={1}
+            >
+              Join millions of travellers around the world and take the journeys that matter. Using our search engine, you can find the best deals on flights, hotels, and car rentals.
+            </MKTypography>
+          </Grid>
+        </Container>
+      </MKBox>
+
+      <Card
+        sx={{
+          p: 2,
+          mx: { xs: 2, lg: 3 },
+          mt: -5,
+          mb: 4,
+          backgroundColor: ({ palette: { white }, functions: { rgba } }) => rgba(white.main, 0.8),
+          backdropFilter: "saturate(200%) blur(30px)",
+          boxShadow: ({ boxShadows: { xxl } }) => xxl,
+        }}
+      >
+
+        The search engine will go here
+        <FormAirBnb handleForm={handleForm} />
+        <div id="data"></div>
+        {/*       <ApartmentList data={response.data.results} />
+ */}
+
+     {response && <ApartmentList data={response.data.results} />}
+      </Card>
+
+
+
+    </div>
+  )
 }
 export default Stays
