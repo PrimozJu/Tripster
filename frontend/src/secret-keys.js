44bbe4e223b0a5ad2c4ae7cac6ab20651c532559
Vito <99677852+Vzupa@users.noreply.github.com>
2023-06-03 18:33:29 +0200

    Create secret-keys.js

---
+++ frontend/src/secret-keys.js
@@ -0,0 +1,15 @@
+export const firebaseKey = {
+    apiKey: "AIzaSyDvrLf6ndKyHMgXCxReOOLXvGRuiW9eOhM",
+    authDomain: "tripsterpraktikum-e913c.firebaseapp.com",
+    projectId: "tripsterpraktikum-e913c",
+    storageBucket: "tripsterpraktikum-e913c.appspot.com",
+    messagingSenderId: "320340062048",
+    appId: "1:320340062048:web:be951119ae91fae5b1b805",
+    measurementId: "G-S1FG8420FV"
+}
+
+export const localApiFlights = "https://europe-west2-tripsterpraktikum-e913c.cloudfunctions.net/app/flights";
+// export const localApiFlights = "http://127.0.0.1:5001/tripsterpraktikum-e913c/europe-west2/app/flights";
+
+export const localApiStays = "https://europe-west2-tripsterpraktikum-e913c.cloudfunctions.net/app/airbnb";
+// export const localApiStays = "http://127.0.0.1:5001/tripsterpraktikum-e913c/europe-west2/app/airbnb";
