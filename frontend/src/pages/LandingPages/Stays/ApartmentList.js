132568a08a3ecc9fa98b7caef5819db0df9954c8
Saso <krepek.saso1@gmail.com>
2023-06-04 19:14:00 +0200

    Posodobljen izgled za airbnb listings

--- frontend/src/pages/LandingPages/Stays/ApartmentList.js
+++ frontend/src/pages/LandingPages/Stays/ApartmentList.js
@@ -1,8 +1,11 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import React from "react";
-import Slider from "react-slick"; // uvozite knjižnico "react-slick" za drsnik slik
+import React, { useContext } from "react";
+import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { Button, Card, CardContent, CardMedia, Grid, Typography } from "@mui/material";
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+import "./ApartmentList.css"; // Import the CSS file for custom styling
+import { UserContext } from "../../../App";
 
 function ApartmentList({ data }) {
   const settings = {
@@ -12,56 +15,86 @@ function ApartmentList({ data }) {
     slidesToScroll: 1,
   };
 
-  console.log("nanaanan");
+  const { cartItems, setCartItems } = useContext(UserContext);
+
+  // Define a function to handle adding an item to the cart
+  const handleAddProduct = (product) => {
+    // Check if the product already exists in the cart
+    const productExist = cartItems.find((item) => item.id === product.id);
+
+    if (productExist) {
+      // If so, update its quantity
+      setCartItems(
+        cartItems.map((item) =>
+          item.id === product.id
+            ? { ...productExist, quantity: productExist.quantity + 1 }
+            : item
+        )
+      );
+    } else {
+      // Otherwise, add it to the cart with a default quantity of 1
+      setCartItems([...cartItems, { ...product, quantity: 1 }]);
+    }
+  };
+
+  const addToCart = (recommendation) => {
+    console.log(recommendation)
+    const { id,  name, price } = recommendation;
+    
+    const item = {
+      name: name,
+      price: price.total,
+      id: id,
+    };
+
+    console.log("Added to cart:", item);
+    handleAddProduct(item);
+  };
+
   return (
     <div className="container">
-      <div className="row row-cols-1 row-cols-md-2 row-cols-lg-4">
+      <Grid container spacing={2}>
         {data.map((result) => (
-          <div key={result.id} className="col mb-4">
-            <div className="card h-100">
+          <Grid key={result.id} item xs={12} sm={6} md={6} lg={4}>
+            <Card sx={{ height: "100%", display: "flex", flexDirection: "column" }}>
               <Slider {...settings}>
                 {result?.images.map((slika, index) => {
                   return (
-                    <div
-                      key={index}
-                      style={{
-                        display: "flex",
-                        justifyContent: "center",
-                        alignItems: "center",
-                      }}
-                    >
-                      <img
-                        className="w-full align-middle rounded-lg"
-                        src={`${slika}`}
-                        style={{
-                          objectFit: "cover",
-                          height: "40vh",
-                          width: "40%",
-                          margin: "auto",
-                        }}
+                    <div key={index}>
+                      <CardMedia
+                        component="img"
+                        alt=""
+                        image={slika}
+                        className="card-image" // Add the CSS class for custom styling
                       />
                     </div>
                   );
                 })}
               </Slider>
-              {/* <img src={result.images[0]} className="card-img-top" alt={result.name} /> */}
-              <div className="card-body">
-                <h5 className="card-title">{result.name}</h5>
-                <p className="card-text">
+              <CardContent sx={{ flexGrow: 1 }}>
+                <Typography variant="h5" component="h2">
+                  {result.name}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
                   <strong>Address:</strong> {result.address}, {result.city}
-                </p>
-                <p className="card-text">
-                  <strong>Price:</strong> {result.price.total}{" "}
-                  {result.price.currency}
-                </p>
-                <a href={result.deeplink} className="btn btn-primary">
-                  Click here to book
-                </a>
-              </div>
-            </div>
-          </div>
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  <strong>Price:</strong> {result.price.total} {result.price.currency}
+                </Typography>
+              </CardContent>
+              <Button
+                variant="contained"
+                color="primary"
+                startIcon={<AddShoppingCartIcon />}
+                onClick={() => addToCart(result)}
+                style={{ color: "#ffffff" }}
+              >
+                Click here to book
+              </Button>
+            </Card>
+          </Grid>
         ))}
-      </div>
+      </Grid>
     </div>
   );
 }
