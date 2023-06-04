17cbf26b84edee59c292e7b200164a514a174d76
Saso <krepek.saso1@gmail.com>
2023-06-04 09:30:46 +0200

    Posodobljen ItineraryDetails

--- frontend/src/pages/LandingPages/Itinirary/ItineraryDetails.js
+++ frontend/src/pages/LandingPages/Itinirary/ItineraryDetails.js
@@ -1,28 +1,48 @@
+import React from "react";
+import { Typography, Card, CardContent, List, ListItem, ListItemText, Grid } from "@mui/material";
+
 const ItineraryDetails = ({ data }) => {
-    console.log(data);
-    if (!data || data.tripArray.length === 0) {
-      return null; // If data or tripArray is undefined or empty, don't display anything
-    }
-  
-    const { travelDestination, tripArray } = data;
-  
-    return (
-      <div>
-        <h1>{travelDestination}</h1>
+  console.log(data);
+  if (!data || data.tripArray.length === 0) {
+    return null; // If data or tripArray is undefined or empty, don't display anything
+  }
+
+  const { travelDestination, tripArray } = data;
+
+  return (
+    <div>
+      <Card variant="outlined" sx={{ marginBottom: 2 }}>
+        <CardContent>
+          <Typography variant="h4" component="h1">
+            {travelDestination}
+          </Typography>
+        </CardContent>
+      </Card>
+      <Grid container spacing={2} sx={{ display: 'flex', flexWrap: 'wrap' }}>
         {tripArray.map((day) => (
-          <div key={day.day}>
-            <h2>Day {day.day}</h2>
-            <p>Description: {day.description}</p>
-            <ul>
-              {day.activities.map((activity, index) => (
-                <li key={index}>{activity}</li>
-              ))}
-            </ul>
-          </div>
+          <Grid key={day.day} item xs={12} md={4}>
+            <Card variant="outlined" sx={{ marginBottom: 4, height: '100%' }}>
+              <CardContent>
+                <Typography variant="h5" component="h3">
+                  Day {day.day}
+                </Typography>
+                <Typography variant="body1" component="p">
+                  <i>{day.description}</i>
+                </Typography>
+                <List>
+                  {day.activities.map((activity, index) => (
+                    <ListItem key={index}>
+                      <ListItemText primary={activity} />
+                    </ListItem>
+                  ))}
+                </List>
+              </CardContent>
+            </Card>
+          </Grid>
         ))}
-      </div>
-    );
-  };
-  
-  export default ItineraryDetails;
-  
+      </Grid>
+    </div>
+  );
+};
+
+export default ItineraryDetails;
